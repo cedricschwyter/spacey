afa6e011a36c93c0aa4fef8e56a7a5929a524859
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-06-17 14:29:35 +0200

    cli: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{App, Arg, ArgMatches};
+
+enum LexicalTokenType {
+    Space,
+    Tab,
+    Linefeed,
+}
+
+fn args() -> ArgMatches {
+    App::new("spacey")
+        .about("a whitespace interpreter")
+        .version("0.1.0")
+        .author("Cedric Schwyter <cedricschwyter@bluewin.ch>")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .takes_value(true)
+                .help("whitespace source file to interpret"),
+        )
+        .get_matches()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = args();
+    let file = args.value_of("file").unwrap();
 }
