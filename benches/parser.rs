c6269bcdf8c2abb72d2137090f02b16ea09fc882
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-01 07:26:01 +0100

    fix:benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use spacey::Parser;
+use spacey::{parser::parser::ParseError, Parser};
 use std::error::Error;
 
 pub fn parse_benchmark(c: &mut Criterion) {
     c.bench_function("parse", |b| {
-        b.iter(|| -> Result<(), Box<dyn Error>> {
+        b.iter(|| -> Result<(), ParseError> {
             let mut parser = Parser::new("ws/quine.ws")?;
             parser.into_iter().for_each(|_instr| {});
 
