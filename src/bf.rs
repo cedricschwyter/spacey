12658ab2d5984aacb4a207895831af88cffdca57
Cedric Schwyter <cedric.schwyter@cleoag.ch>
2022-12-02 18:47:18 +0100

    feat:implement parser interface

--- src/bf.rs
+++ src/bf.rs
@@ -1,5 +1,10 @@
+use crate::parser::ParseErrorKind;
+#[cfg(not(target_arch = "wasm32"))]
 use memmap::Mmap;
+#[cfg(not(target_arch = "wasm32"))]
+use std::fs::File;
 
+use crate::ParseError;
 use crate::Parser;
 
 const R_ANGLE: u8 = b'>';
@@ -23,5 +28,37 @@ pub struct BfParser {
 impl Parser for BfParser {
     fn instruction(&mut self) -> Option<Result<Box<dyn crate::Instr>, crate::ParseError>> {
         None
+    }
+}
+
+impl BfParser {
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn new(file_name: &str) -> Result<Box<dyn Parser>, ParseError> {
+        let file = match File::open(&file_name) {
+            Ok(content) => content,
+            Err(err) => return ParseErrorKind::FileOpenError(Box::new(err)).throw(),
+        };
+        let source = unsafe {
+            match Mmap::map(&file) {
+                Ok(content) => content,
+                Err(err) => return ParseErrorKind::MemoryMapError(Box::new(err)).throw(),
+            }
+        };
+        let index = 0;
+
+        Ok(Box::new(BfParser {
+            source,
+            token_index: index,
+        }))
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    pub fn new(source: &str) -> Result<Box<dyn Parser>, ParseError> {
+        let index = 0;
+
+        Ok(Box::new(BfParser {
+            source: source.to_string().as_bytes().to_vec(),
+            token_index: index,
+        }))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod parser;
 pub mod vm;
 pub mod ws;
 
+pub use bf::BfParser;
 pub use ir::Instruction;
 pub use parser::{Instr, ParseError, Parser, SourceType};
 pub use vm::{Vm, VmConfig, VmError};
